9a201c226681bd7e0d8c2f2d4db3e33fb16c8ccc
Gustavo ProPrinter <suporte@proprinter.com.br>
2025-05-22 13:42:41 -0300

    Corrigido para rodar no Render usando porta dinâmica

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def avisar():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
